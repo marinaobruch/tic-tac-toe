b3476759b14870ddc15a5508e5e804d9aa8c7538
Marina Obruch <marina_obruch@mail.ru>
2023-08-08 23:11:30 +0500

    using useState for re-render board

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { useState } from "react";
 import "./Square.css";
 
-export default function Square({ value }) {
-  return <button className="square">{value}</button>;
+export default function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
