b500f974db59feaef7535284a04e52757210054f
Marina Obruch <marina_obruch@mail.ru>
2023-08-13 10:03:24 +0500

    Added "Status" on site

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { useState } from "react";
 import Square from "../Square/Square";
+import "./Board.css";
 
 export default function Board() {
   const [xIsNext, steXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const winner = calculateWinner(squares);
+
+  let status;
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
@@ -22,9 +26,16 @@ export default function Board() {
     steXIsNext(!xIsNext);
   }
 
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <div>
       <div className="App">
+        <div className="status">{status}</div>
         <div className="board-row">
           <Square
             value={squares[0]}
@@ -70,4 +81,24 @@ export default function Board() {
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
