c59e9cf7b464bf6c4cdb7b36bbf2f5800b53626a
Marina Obruch <marina_obruch@mail.ru>
2023-08-14 08:50:22 +0500

    All logic moved in Game component

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { useState } from "react";
 import Square from "../Square/Square";
 import "./Board.css";
+import calculateWinner from "../../CalcWinner";
 
-export default function Board() {
-  const [xIsNext, steXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+export default function Board({ xIsNext, squares, onPlay }) {
   const winner = calculateWinner(squares);
-
   let status;
 
   function handleClick(i) {
@@ -22,8 +19,7 @@ export default function Board() {
       nextSquares[i] = "O";
     }
 
-    setSquares(nextSquares);
-    steXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   if (winner) {
@@ -81,24 +77,4 @@ export default function Board() {
       </div>
     </div>
   );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -4,10 +4,24 @@ import Board from "../Board/Board";
 import "./Game.css";
 
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+
+  const correntSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board
+          xIsNext={xIsNext}
+          squares={correntSquares}
+          onPlay={handlePlay}
+        />
       </div>
       <div className="game-info">
         <ol>{/*TODO*/}</ol>
